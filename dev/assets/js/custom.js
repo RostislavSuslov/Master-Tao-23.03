3bc75cde29b65d76d20d0d4694a21ffaab1172b1
RostSuslov <suslovros@gmail.com>
2023-06-15 21:25:58 +0300

    header done / first-screen dektop

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,9 @@
-  
+$(document).ready(function(){
+
+    $('.first-screen-slider').slick({
+        dots: true,
+        infinite: true,
+        appendArrows: '.first-screen-slider__arrows'
+    });
+
+});
