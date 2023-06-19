87bcfaedf3cd32e535048f18637c33fe2c57e1da
RostSuslov <suslovros@gmail.com>
2023-06-19 21:32:44 +0300

    first screen done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     $('.first-screen-slider').slick({
         dots: true,
         infinite: true,
-        appendArrows: '.first-screen-slider__arrows'
+        appendArrows: '.first-screen-slider__arrows',
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    arrows: false,
+                }
+            },
+
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
 });
