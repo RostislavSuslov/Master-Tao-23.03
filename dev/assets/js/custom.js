8ab1e6a07671e11b0f014dd3563adcbaef1b58af
RostSuslov <suslovros@gmail.com>
2023-06-22 21:40:07 +0300

    form start

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -18,3 +18,53 @@ $(document).ready(function() {
     });
 
 });
+
+
+const checkbox = document.querySelectorAll('[type="checkbox"]')
+
+
+console.log(document.querySelectorAll('[type="checkbox"]'));
+
+checkbox.forEach(item => {
+    item.closest('.input-box').classList.add('has-checkbox')
+    item.closest('.input-column').classList.add('padding-top')
+})
+
+document.getElementById('myfile').onchange = function() {
+
+    //long name
+
+
+
+
+    const fileName = document.getElementById('file-name');
+    fileName.innerHTML = this.value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //short name
+    // document.getElementById('file-name').innerHTML = this.files[0].name;
+
+};
