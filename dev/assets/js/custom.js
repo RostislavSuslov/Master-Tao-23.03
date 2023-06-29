ce9ec50c8dffc25c84d0b331e651cb66f25bab27
RostSuslov <suslovros@gmail.com>
2023-06-29 21:20:24 +0300

    yoksel

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.first-screen-slider').slick({
         dots: true,
         infinite: true,
-        appendArrows: '.first-screen-slider__arrows',
+        appendArrows: '.first-screen-slider-arrows',
         responsive: [{
                 breakpoint: 991,
                 settings: {
